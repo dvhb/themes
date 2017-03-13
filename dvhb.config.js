918e7d91b33c5f1acfeb91256fe449d59d083ebd
Aleksey Subbotenko <asu@dvhb.ru>
2017-03-13 15:17:18 +0300

    Prepare repo for npm publish package

--- dvhb.config.js
+++ dvhb.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
@@ -11,7 +12,13 @@ module.exports = {
     extendWebpackConfig(webpackConfig) {
         const dir = path.resolve(__dirname, 'src');
 
-
+        webpackConfig.output.filename = '[name].js';
+        webpackConfig.output.chunkFilename = '[name].js';
+        webpackConfig.plugins.forEach(function (plugin, i) {
+            if (plugin.filename === '[name].[contenthash].css') {
+                webpackConfig.plugins[i] = new ExtractTextPlugin('[name].css');
+            }
+        });
 
         // webpackConfig.module.loaders.push(
         //     {
--- src/index.js
+++ src/index.js
@@ -3,18 +3,3 @@
  */
 
 import './styles';
-import './app';
-
-import APP_ENV from 'webpack-config-loader!./config';
-
-console.log('APP_ENV', APP_ENV);
-console.log('VERSION', VERSION);
-console.log('COMMITHASH', COMMITHASH);
-
-import Modernizr from 'modernizr';
-console.log(Modernizr);
-
-//use it for enable HRM
-// if (module.hot) {
-//     module.hot.accept();
-// }
